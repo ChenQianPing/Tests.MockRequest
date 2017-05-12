29793555b13f815f8ba4dede0e5cc38968ffb3c7
chenqp <13542427@qq.com>
2017-05-12 17:28:06 +0800

    First Commit

---
+++ Tests.MockRequest/ConcurrentHelper.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Tests.MockRequest
+{
+    public class ConcurrentHelper
+    {
+        public int TaskId { get; private set; }
+
+        public ConcurrentHelper(int seed)
+        {
+            TaskId = seed;
+        }
+
+        public int ReceiveData()
+        {
+            const string url = "http://192.168.2.100:9001/api/Rvw/GetImgPart/00AA44995CA94A6FB577E5D9A6F4B1E0/B477847E59AF4E03A37C12B15EFF6ADE";
+
+            try
+            {
+                using (var webclient = new WebClient())
+                {
+                    webclient.DownloadFile(url, $@"E:\10001003\temp\{Guid.NewGuid():N}.jpg");
+                    return TaskId;
+                }
+            }
+            catch (Exception)
+            {
+                return -1;
+            }
+        }
+    }
+}
+
--- Tests.MockRequest/Program.cs
+++ Tests.MockRequest/Program.cs
@@ -1,7 +1,9 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Diagnostics;
 using System.Linq;
 using System.Text;
+using System.Threading;
 using System.Threading.Tasks;
 
 namespace Tests.MockRequest
@@ -10,6 +12,42 @@ namespace Tests.MockRequest
     {
         static void Main(string[] args)
         {
+            const int maxThread = 100;
+            BeginTest(maxThread);
+        }
+
+        public static void BeginTest(int maxThread)
+        {
+            for (var i = 0; i < maxThread; i++)
+            {
+                var task = new ConcurrentHelper(i);
+                new Thread(ThreadFun) { IsBackground = true }.Start(task);
+            }
+            Console.ReadLine();
+        }
+
+        private static void ThreadFun(object o)
+        {
+            var t = o as ConcurrentHelper;
+            var count = 0;
+            while (true)
+            {
+                count++;
+                var stopwatch = new Stopwatch();
+                stopwatch.Start();
+                if (t != null)
+                {
+                    var time = t.ReceiveData();
+                    stopwatch.Stop();
+                    if (time == -1)
+                    {
+                        Console.WriteLine($"线程ID：{t.TaskId}第{count}次接收失败！");
+                    }
+                }
+                if (t != null)
+                    Console.WriteLine($"线程ID：{t.TaskId}第{count}次接收成功！耗时：{Convert.ToInt16(stopwatch.Elapsed.TotalMilliseconds)}毫秒!");
+                Thread.Sleep(500);
+            }
         }
     }
 }
